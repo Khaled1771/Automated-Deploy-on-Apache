4e17bb49b9de031b5b4970effde5b689dcf1e5a0
Khaled Mahmoud <khhaledd.77@gmail.com>
2024-08-23 19:03:50 +0300

    functions file for Jenkins

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def Build(){
+    echo "Building the app"
+}
+
+def Test(){
+    echo "Testing the app"
+}
+
+def Deploy(){
+    echo "Deploying the app"
+    echo "Deploying verion ${params.VERSION}"
+}
+
+return this
